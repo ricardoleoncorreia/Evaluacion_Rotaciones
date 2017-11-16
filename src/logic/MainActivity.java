1293c4fa4a95711246961267eb7cfceb010815ac
Ricardo Leon <ricardoleoncorreia@gmail.com>
2017-11-16 19:11:20 -0400

    Handle all exceptions

--- src/logic/MainActivity.java
+++ src/logic/MainActivity.java
@@ -6,68 +6,110 @@ import java.util.Scanner;
 public class MainActivity {
 
 	private static final String INPUT_FIRST_LINE = "Ingrese la primera línea:";
-	private static final String INPUT_SECOND_LINE = "Ingrese la primera línea:";
+	private static final String INPUT_SECOND_LINE = "Ingrese la segunda línea:";
+	private static final String EMPTY_LINE_MESSAGE = "La línea debe contener únicamente números enteros";
+	private static final String INDEX_OUT_OF_BOUNDS_MESSAGE = "La longitud seleccionada (n) no coincide con la longitud de la matriz en la segunda línea";
+	private static final String N_CONDITION_MESSAGE = "El valor del primer número (n) debe estar entre 1 y 100.000";
+	private static final String D_CONDITION_MESSAGE = "El valor del segundo número (d) debe estar entre 1 y n";
+	private static final String AI_CONDITION_MESSAGE = "Cada elemento de la matriz debe estar entre 1 y 1.000.000";
 	
 	public static void main(String[] args) {
-		System.out.println(INPUT_FIRST_LINE);
 		Scanner in = new Scanner(System.in);
-		String firstLine = in.nextLine();
-		//Checking first input
-		if (isValidLine(firstLine)){
+		try{
+			System.out.println(INPUT_FIRST_LINE);
+			String firstLine = in.nextLine();
+			//Checking first input
+			isValidLine(firstLine);
+			//Checking requirements for first line
+			ArrayList<Integer> firstLineArray = convertToArray(firstLine);
+			int n = firstLineArray.get(0);
+			int d = firstLineArray.get(1);
+			checkRequirements(n, d);
 			System.out.println(INPUT_SECOND_LINE);
 			String secondLine = in.nextLine();
 			//Checking second input
-			if (isValidLine(secondLine)){
-				//Convert inputs in arrays
-				ArrayList<Integer> arrayFirstLine = convertToArray(firstLine);
-				ArrayList<Integer> array = convertToArray(secondLine);
-				//Getting number of operations
-				int d = arrayFirstLine.get(1);
-				//Rotating operation
-				for (int i = 0; i < d; i++){
-					array.add(array.get(0));
-					array.remove(0);
-				}
-				//String Builder to show result
-				StringBuilder sb = new StringBuilder();
-				for (int a : array)
-				{
-				    sb.append(a);
-				    sb.append(" ");
-				}
-				System.out.println(sb.toString());		
-			}else{
-				System.out.println("Error en la fila 2");
-			}			
-		}else{
-			System.out.println("Error en la fila 1");
+			isValidLine(secondLine);
+			ArrayList<Integer> array = convertToArray(secondLine);
+			checkRequirements(array);
+			//Rotating operation
+			for (int i = 0; i < d; i++){
+				array.add(array.get(0));
+				array.remove(0);
+			}
+			//String Builder to show result
+			StringBuilder sb = new StringBuilder();
+			for (int a : array)
+			{
+			    sb.append(a);
+			    sb.append(" ");
+			}
+			System.out.println(sb.toString());
+		}catch (IllegalArgumentException e){
+			//If a line is empty, show
+			System.out.println(EMPTY_LINE_MESSAGE);
+		}catch (IndexOutOfBoundsException e){
+			//If a line is empty, show
+			System.out.println(INDEX_OUT_OF_BOUNDS_MESSAGE);
+		}catch (nConditionException e){
+			//If a line is empty, show
+			System.out.println(N_CONDITION_MESSAGE);
+		}catch (dConditionException e){
+			//If a line is empty, show
+			System.out.println(D_CONDITION_MESSAGE);
+		}catch (aiConditionException e){
+			//If a line is empty, show
+			System.out.println(AI_CONDITION_MESSAGE);
+		}finally{
+			//Before program finishes, close Scanner
+			in.close();
 		}
-		in.close();
+	}
+	
+	/*This method checks is an input line is valid
+	 * @param line: line to check 
+	 */
+	private static void isValidLine(String line){
+		//Checking that input is not empty or full of blank spaces
+		if (line.equals("") || line.replaceAll(" ", "").equals("")){
+			throw new IllegalArgumentException();
+		}
+		/*Checking if each element in line is an integer
+		 * When a NumberFormatException appears, IllegalArgumentException will handle it
+		 */
+		Scanner scanner = new Scanner(line);
+		while (scanner.hasNext()){
+			Integer.parseInt(scanner.next());
+		}
+		//Close the scanner and return true
+		scanner.close();
 		
 	}
 	
-	private static boolean isValidLine(String line){
-		//Checking that input is not empty or full of blank spaces
-		if (line.equals("") || line.replaceAll(" ", "").equals("")){
-			return false;
+	/*This method checks for first line input requirements
+	 * @param n: length of the array
+	 * @param d: number of rotations
+	 */
+	private static void checkRequirements(int n, int d) throws nConditionException, dConditionException{
+		if (n < 1 || n > 100000){
+			throw new MainActivity().new nConditionException();
 		}
-		//Checking if each character in line is an integer
-		Scanner scanner = new Scanner(line);
-		while (scanner.hasNext()){
-			try{
-				Integer.parseInt(scanner.next());
-			}catch (NumberFormatException nfe){
-				//If false, return false as validation result and close the scanner
-				System.out.println("NumberFormatException");
-				scanner.close();
-				return false;
-			}
+		if (d < 1 || d > n){
+			throw new MainActivity().new dConditionException();
 		}
-		//Successful validation, close the scanner and return true
-		scanner.close();
-		return true;
 	}
 	
+	/*This method checks for second line input requirements
+	 * @param array: array to rotate 
+	 */
+	private static void checkRequirements(ArrayList<Integer> array) throws aiConditionException{
+		for (int element : array){
+			if (element < 1 || element > 1000000){
+				throw new MainActivity().new aiConditionException();
+			}
+		}
+	}
+	
+	// This method helps to convert the line into an array
 	private static ArrayList<Integer> convertToArray(String input){
 		ArrayList<Integer> array = new ArrayList<Integer>();
 		Scanner in = new Scanner(input);
@@ -83,6 +125,9 @@ public class MainActivity {
 	 *For this exercise, only no-argument constructor is needed 
 	 */
 	private class nConditionException extends Exception{
+		
+		private static final long serialVersionUID = 1L;
+
 		public nConditionException() { 
 			super(); 
 		}
@@ -92,6 +137,9 @@ public class MainActivity {
 	 * For this exercise, only no-argument constructor is needed
 	 */
 	private class dConditionException extends Exception{
+		
+		private static final long serialVersionUID = 1L;
+
 		public dConditionException() { 
 			super(); 
 		}
@@ -101,6 +149,9 @@ public class MainActivity {
 	 * For this exercise, only no-argument constructor is needed
 	 */
 	private class aiConditionException extends Exception{
+		
+		private static final long serialVersionUID = 1L;
+
 		public aiConditionException() { 
 			super(); 
 		}
